137b9e4348bfe2cd793f20da2a0b1263110c51a4
Cetok <jtorvisco42@gmail.com>
2024-09-09 09:44:01 -0500

    database

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace repasodb.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<repasodb.Models.Cliente> DataCliente { get;set;}
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace repasodb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
